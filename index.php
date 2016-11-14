8dde3dde6b8a47220563ae3d79312822b0cc80fb
José Leonardo Cavalcante da Penha <joseleonardocavalcante2014@gmail.com>
2016-11-14 16:44:53 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -7,11 +7,27 @@
 </head>
 
 <body>
+
 <header id="cabeçalho">//Cabeçalho
+
     <span id="Cabeçalho"><h1>People Universe</h1></span>
 
 </header>
-<span id="titulo2"><h6>O ponto de partida da evolução _</h6></span>
+
+<span id="titulo2"><h6>O ponto de partida da evolução<span class="blink"> _</span></h6></span>
+
+<section>
+    <fieldset id="login">
+        <img src="imgs/planeta.png" id="icone">
+        <br>
+        <span id="entrar">Entrar</span>
+        <form id="login" action="#" method="post">
+            
+
+        </form>
+    </fieldset>
+</section>
+
 
 
 
@@ -20,5 +36,6 @@
 <footer>
     Todos os
 </footer>
+
 </body>
 </html>
