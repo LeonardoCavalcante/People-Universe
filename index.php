26748ab9ff4127fd026cbfbfa99bdbc86d4e624b
José Leonardo Cavalcante da Penha <joseleonardocavalcante2014@gmail.com>
2016-11-14 14:30:56 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="estilo.css">
+    <title>Exemplo 1</title>
+</head>
+
+<body>
+<header>
+  <span id="Cabeçalho"><h1>People Universe</h1></span>
+    <hr>
+</header>
+</body>
+</html>
